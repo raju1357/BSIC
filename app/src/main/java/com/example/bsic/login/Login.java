362a1984cd95caeb29836c62b513d7f723600c0c
Raju <rajubxr.07@gmail.com>
2020-03-18 01:37:15 +0530

    first commit & login UI

---
+++ app/src/main/java/com/example/bsic/login/Login.java
@@ -0,0 +1,18 @@
+package com.example.bsic.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bsic.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+}
